e752a34c79569d4e09586181bafc838aa8da1035
flyysr <flyysr@gmail.com>
2016-07-06 15:53:21 +0800

    'update'

--- app/config/config.php
+++ app/config/config.php
@@ -1,1 +1,10 @@
 <?php
+return [
+    'DB'=>[
+        'host' => '172.17.92.51',
+        'port' => '5432',
+        'db'   => 'm3vg',
+        'user' => 'postgres',
+        'password' => 'postgres'
+    ]
+];
